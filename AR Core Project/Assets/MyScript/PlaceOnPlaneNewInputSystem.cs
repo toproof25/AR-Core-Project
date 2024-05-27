e642babb52d7e19e300ef30972ce1b4add2afd0d
Embeu <kmnlmn123@gmail.com>
2024-05-27 15:36:14 +0900

    초기화 후 업로드

---
+++ AR Core Project/Assets/MyScript/PlaceOnPlaneNewInputSystem.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlaneNewInputSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("클릭 시 나타나는 오브젝트 프리팹")]
+    GameObject placedPrefab;
+
+    Touch_control controls;
+
+    ARRaycastManager aRRaycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    List<GameObject> box = new List<GameObject>();
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+
+        // 만든 터치 컨트롤 가져와서 이벤트 연결
+        controls = new Touch_control();
+
+        controls.control.touch.performed += _ => {
+            var touchPosition = Pointer.current.position.ReadValue();
+            OnPress(touchPosition);
+        }; 
+
+        controls.control.touch.canceled += _ => { 
+            //
+        }; 
+    }
+
+    private void OnPress(Vector3 position)
+    {
+
+        if (aRRaycastManager.Raycast(position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            // 첫번째로 닿은 raycast hit 오브젝트
+            var hitPose = hits[0].pose;
+            GameObject spawnedObject = null;
+
+            // 생성된 오브젝트의 개수가 5개로 제한하는 코드
+            if (box.Count < 5)
+            {
+                spawnedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                box.Add(spawnedObject);
+            }
+            else
+            {
+                // 5개를 초과하는 생성일 경우, 1번째 오브젝트를 제거 후 오브젝트를 Add함
+                Destroy(box[0]);
+                box.RemoveAt(0);
+
+                spawnedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                box.Add(spawnedObject);
+            }
+
+            // 생성된 오브젝트 방향을 내 방향으로
+            Vector3 lookPos = Camera.main.transform.position - spawnedObject.transform.position;
+            lookPos.y = 0;
+            spawnedObject.transform.rotation = Quaternion.LookRotation(lookPos);
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.control.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.control.Disable();
+    }
+
+}
